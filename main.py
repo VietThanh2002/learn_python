9c3b4128aa686814bfff973d141735eeb0851aa4
VietThanh2002 <114978891+VietThanh2002@users.noreply.github.com>
2024-02-04 11:23:06 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# print ("Hello World")
+# print ("I am Viết Thanh")
+
+# a = 10
+# b = 20
+
+# c = a + b
+
+# print(c)
+
+# name = 'Thanh'
+
+# print("Hello" + " " + name)
+
+first_name = 'Phạm' + " "
+last_name = 'Viết Thanh' + " "
+full_name = first_name + last_name
+age = 22
+
+# print(first_name + " " + last_name)
+print("Hello" + " " + full_name + " " + str(age) + "year old")
---
+++ string.py
@@ -0,0 +1,13 @@
+name = "thanh Pham"
+
+#  Tìm kiểm 1 ký tự trong chuổi
+print(name.find("h"))
+
+#Đếm só ký tự trong chuổi
+print(name.count("h"))
+
+# In ra chuỗi với chữ cái đầu viết in hoa
+print(name.capitalize())
+
+# in ra chuổi in hoa
+print(name.upper())
