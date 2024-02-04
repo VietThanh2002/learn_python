3b5e4893ef34af39078e853f29e965d1d6ecd8db
VietThanh2002 <114978891+VietThanh2002@users.noreply.github.com>
2024-02-04 12:08:51 +0700

    if_else

---
+++ if_else.py
@@ -0,0 +1,14 @@
+# age = int(input("How old are you ?:" ));
+
+# if age >= 18:
+#     print('Bạn đã trưởng thành')
+# else: 
+#     print('Con nít')
+
+temp = int(input("How many degrees Celsius is the weather today?: "))
+
+if temp >= 0 and temp <=30:
+    print("The weather is very nice ! We go to stadium !")
+elif temp < 0 or temp > 30:
+    print("The weather is very bad ! We should stay at home")
+
